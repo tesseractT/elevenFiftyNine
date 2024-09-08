beb6cd7383c00d3704fb71c398333d29a483db8d
tesseractT <ebere.benny97@gmail.com>
2024-09-08 13:51:23 +0100

    admin sidebar active

---
+++ app/Helper/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/** Set Sidebar item active */
+
+
+function setActive(array $route)
+{
+    if (is_array($route)) {
+        foreach ($route as $r) {
+            if (request()->routeIs($r)) {
+                return 'active';
+            }
+        }
+    }
+}
